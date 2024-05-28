cabfd435995718dc728639cf1f1539aa2c20ac2c
Bruno Gonçalves <86898405+brunogonca@users.noreply.github.com>
2024-05-28 17:10:36 +0000

    added scripts `lint:check` and `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 This is a clone of a famous brazilian newsletter, a personal project to improve my programming skills.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>Eu amo muito minha família e meu amigos</h1>
+  return <h1>Eu amo muito minha família e meu amigos</h1>;
+}
+
+function teste1() {
+  console.log("teste");
 }
 
 export default Home;
