f5eedb37b709768d5b18c2d620256083cf5e8e76
Bruno Gonçalves <86898405+brunogonca@users.noreply.github.com>
2024-06-20 21:26:14 +0000

    make

--- infra/database.js
+++ infra/database.js
@@ -9,9 +9,15 @@ async function query(QueryObject) {
     password: process.env.POSTGRES_PASSWORD,
   });
   await client.connect();
-  const result = await client.query(QueryObject);
-  await client.end();
-  return result;
+
+  try {
+    const result = await client.query(QueryObject);
+    return result;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await client.end();
+  }
 }
 
 export default {
