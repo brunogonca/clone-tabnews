e0a26992d5ae6306138e595633dc2eeaa91209de
Bruno Gonçalves <86898405+brunogonca@users.noreply.github.com>
2024-06-27 20:47:48 +0000

    fixing branches

--- infra/scripts/wait-for-postgres.js
+++ infra/scripts/wait-for-postgres.js
@@ -1,19 +1,34 @@
 const { exec } = require("node:child_process");
 
+const messageWaiting = "Waiting for Postgres!";
+const messageReady = "Postgres Ready!";
+
+const startedAt = Date.now();
+function showElapsedTime() {
+  return `${((Date.now() - startedAt) / 1000).toFixed(2)}s`;
+}
+
+function showLoading() {
+  const intervalToUpdateMs = 200;
+  const emojis = ["🌑", "🌒", "🌓", "🌔", "🌕", "🌖", "🌗", "🌘"];
+  const index = Math.floor(Date.now() / intervalToUpdateMs) % emojis.length;
+  return `${showElapsedTime()} ${emojis[index]}`;
+}
+
 function checkPostgres() {
   exec("docker exec postgres-dev pg_isready --host localhost", handleReturn);
-
   function handleReturn(error, stdout) {
     if (stdout.search("accepting connections") === -1) {
-      process.stdout.write(".");
+      process.stdout.write(`\r ${messageWaiting} ${showLoading()}`);
       checkPostgres();
       return;
     }
 
-    console.log("\n\n**Postgres aceitando conexões\n");
+    process.stdout.write(`\r 🌚${messageWaiting} - ${showElapsedTime()}`);
+    process.stdout.write(`\n 🌞${messageReady}\n`);
   }
 }
 
-process.stdout.write("\n\n* Aguardando postgres aceitar conexões");
+process.stdout.write(`\n\n🌚${messageWaiting}`);
 
 checkPostgres();
